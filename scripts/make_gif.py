dda7b1a373b3f191a2d9baeff6d53b946e42b23e
Alexander Smirnov <ru.alexander.smirnov@gmail.com>
2019-11-13 13:42:29 +0300

    Update make_gif.py

--- scripts/make_gif.py
+++ scripts/make_gif.py
@@ -11,13 +11,13 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--text", type=str,
-                        help="Text that you want to make .gif of.",
+                        help="text that you want to make .gif of",
                         default="Sweetheart, I miss you so much")
     parser.add_argument("-p", "--pictures", type=str,
-                        help="Path to folder with pictures that will be used for painting letters.",
+                        help="path to folder with pictures that will be used for painting letters",
                         default="../support_files/pictures_to_write_letters/")
     parser.add_argument("-o", "--output", type=str,
-                        help="Path to folder that .gif will be saved.",
+                        help="path to folder that .gif will be saved",
                         default="../output/")
     args = parser.parse_args()
     generate_gif(text=args.text,
